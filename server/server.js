f8def4b051c889cd121f8960940333d951e88c38
takai <nipas2013@gmail.com>
2022-02-17 21:29:33 +0700

    update : server.js add route for room exit

--- server/server.js
+++ server/server.js
@@ -12,6 +12,25 @@ const server = http.createServer(app);
 
 app.use(cors());
 
+let connectedUsers = [];
+let rooms = [];
+
+app.get('/api/room_exits/:roomId', (req, res, next) => {
+    const { roomId } = req.params;
+    const room = rooms.find((room) => room.id === roomId);
+
+    if (room) {
+        if (room.connectedUsers.length > 3) {
+            return res.send({ roomExists: true, full: true });
+        }
+        else {
+            return res.send({ roomExists: true, full: false });
+        }
+    } else {
+        return res.send({ roomExists: false });
+    };
+});
+
 const io = require('socket.io')(server, {
     cors : {
         origin: '*',
