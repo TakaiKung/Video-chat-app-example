83fab397cedafedcce43f1c0f442968b2d8bc8b1
takai <nipas2013@gmail.com>
2022-02-18 16:22:25 +0700

    add : camera button

--- ui/src/RoomPage/VideoSection/CameraButton.js
+++ ui/src/RoomPage/VideoSection/CameraButton.js
@@ -1,7 +1,22 @@
+import React, { useState } from 'react';
+import CameraButtonImg from '../../resources/camera.svg';
+import CameraButtonImgOff from '../../resources/cameraOff.svg';
+
 const CameraButton = () => {
+    const [isLocalVideoDisabled, setIsLocalVideoDisabled] = useState(false);
+
+    const handleCameraButtonPressed = () => {
+        setIsLocalVideoDisabled(!isLocalVideoDisabled);
+    };
+
     return (
-        <div>
-            
+        <div className='video_button_container'>
+            <img
+                src={ isLocalVideoDisabled ? CameraButtonImgOff : CameraButtonImg }
+                className='video_button_image'
+                onClick={handleCameraButtonPressed}
+                alt={'video'}
+            />
         </div>
     );
 }
