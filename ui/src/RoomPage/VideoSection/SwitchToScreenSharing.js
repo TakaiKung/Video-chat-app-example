1fa2b2ed252ee1e2e1aec89b0bb6325c1137ecb9
takai <nipas2013@gmail.com>
2022-02-18 16:42:04 +0700

    add : screen share button

--- ui/src/RoomPage/VideoSection/SwitchToScreenSharing.js
+++ ui/src/RoomPage/VideoSection/SwitchToScreenSharing.js
@@ -1,7 +1,20 @@
-const SwitchToScreenSharing = () => {
-    return (
-        <div>
+import React, { useState } from "react";
+import SwitchImg from '../../resources/switchToScreenSharing.svg';
 
+const SwitchToScreenSharing = () => {
+    const [isScreenSharingActive, setIsScreenSharingActive] = useState(false);
+
+    const handleScreenShareToggle = () => {
+        setIsScreenSharingActive(!isScreenSharingActive);
+    };
+
+    return (
+        <div className="video_button_container">
+            <img
+                src={ SwitchImg }
+                onClick={handleScreenShareToggle}
+                alt={'sharescreen'}
+            />
         </div>
     );
 }
